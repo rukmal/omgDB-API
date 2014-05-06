58f374b50805d1e394d4bf706c1536b60436b4ef
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-06 15:56:33 -0700

    Added some comments, and rmeoved the old index file

--- openmusicgenome/db.py
+++ openmusicgenome/db.py
@@ -6,7 +6,18 @@ READ_ONLY_USER = 'read'
 READ_ONLY_PASSWORD = '123'
 
 class OpenMusicDB(Object):
-	def __init__(self, user, password):
-		dburl = DATABASE_URL_PREFIX + user + ':' + password + DATABASE_URL_SUFFIX
+	def __init__(self, **kwargs):
+		'''Function to initialize a new omgDB session
+		Args:
+			
+		'''
+		dburl = ''
+		try:
+			dburl = DATABASE_URL_PREFIX + kwargs['user'] + ':' + kwargs['password'] + DATABASE_URL_SUFFIX
+		except:
+			dburl = DATABASE_URL_PREFIX + READ_ONLY_USER  + ':' + READ_ONLY_PASSWORD + DATABASE_URL_SUFFIX
 		client = MongoClient(dburl)
 		self.db = client['openmusicdata']
+		print kwargs
+
+	def authenticate()
