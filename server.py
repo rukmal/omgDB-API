939d3ca345787426ecb4cc1894da8203a8a2d3b3
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-05 22:09:21 -0700

    Changed name

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
-from jinja2 import Template
+from flask import Flask, request
+
 from flask.ext.restful import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 api = Api(app)
 
 # Handling base page routing
 @app.route('/')
 def landingpage():
-	return render_template('docs/developers.html')
+	return app.send_static_file('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='localhost', port=3000)
