8fadb22916c035e27505c29f590a214d8ccb80f6
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-09 12:16:19 -0700

    Made some minor changes to the landing page, and added redis to the requirements

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 from flask import Flask, request
+import redis
 
 from flask.ext.restful import Resource, Api
 
 app = Flask(__name__, static_url_path='')
 api = Api(app)
 
-# Handling base page routing
+# Handling landing page routing
 @app.route('/')
 def landingpage():
 	return app.send_static_file('index.html')
+
+# Handling the generation of new IDs
+@app.route('/gettoken', methods=['POST'])
+def authtoken():
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='localhost', port=3000)
