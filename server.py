26196cec859b2f8dd205df7891aa91815a51e592
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-10 14:52:26 -0700

    Added some redis connection code

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from flask.ext.restful import Resource, Api
 import redis
 
-from flask.ext.restful import Resource, Api
+# Connecting to the redis store (for api keys)
+red = redis.StrictRedis(host='pub-redis-19603.us-east-1-4.3.ec2.garantiadata.com', port=19603, db=0)
 
 app = Flask(__name__, static_url_path='')
 api = Api(app)
@@ -12,9 +14,8 @@ def landingpage():
 	return app.send_static_file('index.html')
 
 # Handling the generation of new IDs
-@app.route('/gettoken', methods=['POST'])
-def authtoken():
-
+# @app.route('/gettoken', methods=['POST'])
+# def authtoken():
 
 if __name__ == '__main__':
 	app.run(debug=True, host='localhost', port=3000)
